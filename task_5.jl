2fd7e97b467fdc9f22489be90f9424ea13cc7136
Олег <146652428+Futeyn@users.noreply.github.com>
2023-12-27 15:24:57 +0300

    Update task_5.jl

--- task_5.jl
+++ task_5.jl
@@ -3,15 +3,9 @@
 перегородки (все перегородки изолированы друг от друга, прямоугольники могут вырождаться в отрезки)
 Результат: Робот - в исходном положении и в углах поля стоят маркеры =#
 
-include("roblib.jl")
-    #=
-        invers(side::HorizonSide)
-        movements!(r::Robot,side::HorizonSide,num_steps::Int)
-        get_num_steps_movements!(r::Robot, side::HorizonSide)
-        movements!(r::Robot,side::HorizonSide)
-        moves!(r::Robot,side::HorizonSide)
-        find_border!(r::Robot,direction_to_border::HorizonSide, direction_of_movement::HorizonSide)
-    =#
+using HorizonSideRobots
+r = Robot(animate=true)
+
 
 function mark_angles(r)
     num_steps=[]
